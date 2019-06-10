13cb39e2ac257e952e542faf1c2d2f56eecf0b6f
Spenser Riches <spenserr@amazon.com>
2019-06-10 11:55:50 -0700

    Updates

---
+++ precache-manifest.bdacf95f0cb2fb830a8098b20b7b6dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba575392862f139cea697d02c2b3d984",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "02cc8e3818c247a9ba0e",
+    "url": "/react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "02cc8e3818c247a9ba0e",
+    "url": "/react-app/static/js/main.4697a7f6.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
